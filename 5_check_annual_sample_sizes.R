2c77dc7fae2ecc42140bde66b8b9013a73c47244
Harrison Cassel <60158937+harrisoncassel@users.noreply.github.com>
2024-03-16 21:42:00 -0400

    correct code

--- 5_check_annual_sample_sizes.R
+++ 5_check_annual_sample_sizes.R
@@ -1,5 +1,4 @@
-# The following vectors already exist:
-## brfss.vars is a character vector with the (string) names of analyzed vars
+# The following vectors already exists:
 ## southeast is a character vector with the (string) names of non-exp. southeast states
 # peer.region (defined below) is char vector of peer region state names
 peer.region <- c('Arkansas', 'Kentucky', 'Michigan', 'Oregon', 'West Virginia')
@@ -14,28 +13,17 @@ list.of.yrs <- list(RAW.BRFSS.14, RAW.BRFSS.18, RAW.BRFSS.22)
 for (YEAR in list.of.yrs) {
   # Get region-specific raw data.frames
   SOUTH.current.yr <- YEAR %>%
-    filter(state %in% southeast)
+    filter(state %in% southeast & med.exp.pop == TRUE) %>%
+    group_by(state) %>%
+    summarise(n=n())
   COHORT.current.yr <- YEAR %>%
-    filter(state %in% peer.region)
-  
-  # Calculate unweighted sample size for each variable
-  for (var in brfss.vars) {
-    # Calculate & store south unweighted sample size
-    SOUTH.current.filtered <- SOUTH.current.yr %>%
-      filter(!is.na(!!sym(var))) %>% # because na.rm = TRUE in svyby/svymean code
-      group_by(state) %>%
-      summarise(n=n())
+    filter(state %in% peer.region & med.exp.pop == TRUE) %>%
+    group_by(state) %>%
+    summarise(n=n())
     
-    # Calculate & store cohort unweighted sample size
-    COHORT.current.filtered <- COHORT.current.yr %>%
-      filter(!is.na(!!sym(var))) %>% # because na.rm = TRUE in svyby/svymean code
-      group_by(state) %>%
-      summarise(n=n())
-    
-    # Update sample size DFs
-    south.sample.sizes <- rbind(south.sample.sizes, SOUTH.current.filtered$n)
-    cohort.sample.sizes <- rbind(cohort.sample.sizes, COHORT.current.filtered$n)
-  }
+  # Update sample size DFs
+  south.sample.sizes <- rbind(south.sample.sizes, SOUTH.current.yr$n)
+  cohort.sample.sizes <- rbind(cohort.sample.sizes, COHORT.current.yr$n)
   
   # Add appropriate row names
   names(south.sample.sizes) <- sort(southeast)
